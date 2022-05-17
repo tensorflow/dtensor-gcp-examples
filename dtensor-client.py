fc0fd3cb3ce22b48a8dbf9ce59edeebeee42ccbc
Yu Feng <feyu@google.com>
2022-05-17 22:13:51 +0000

    first files.

---
+++ dtensor-client.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow.experimental import dtensor
+print('client', dtensor.client_id(), 'devices', tf.config.list_physical_devices('GPU'))
+print(tf.__version__)
+dtensor.initialize_multi_client()
+mesh = dtensor.create_distributed_mesh([("batch", 4)], device_type='GPU', num_global_devices=4)
+
+def main():
+    layout = dtensor.Layout(['batch', dtensor.UNSHARDED], mesh)
+    data = dtensor.call_with_layout(tf.ones, layout, shape=(4, 100))
+    print(data)
+
+main()
---
+++ run.sh
@@ -0,0 +1,13 @@
+export TF_CPP_MIN_LOG_LEVEL=3
+export DTENSOR_JOB_NAME=worker
+export DTENSOR_JOBS=localhost:9890,localhost:9891,localhost:9892,localhost:9893
+export DTENSOR_NUM_CLIENTS=4
+
+for i in 0 1 2 3; do
+(
+export CUDA_VISIBLE_DEVICES=${i}
+export DTENSOR_CLIENT_ID=${i}
+python dtensor-client.py
+) &
+done
+wait
