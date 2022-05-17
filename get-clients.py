167bf8f5a644884a5f692b04ea63a5e9356650dc
Yu Feng <feyu@google.com>
2022-05-17 23:17:09 +0000

    typo

--- get-clients.py
+++ get-clients.py
@@ -6,7 +6,7 @@ dtensor_jobs = os.environ['DTENSOR_JOBS']
 
 dtensor_jobs = dtensor_jobs.split(',')
 
-if len(sys.argv) == 1:
+if len(sys.argv) == 2:
   hostname = sys.argv[1]
   for i, job in enumerate(dtensor_jobs):
     host, port = job.split(":")
