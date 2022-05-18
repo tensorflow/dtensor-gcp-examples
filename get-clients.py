18bcdf9f4993ebd75380bcadb5f8e861fcfdbf12
Yu Feng <feyu@google.com>
2022-05-18 00:13:24 +0000

    expect dtensor-jobs from /etc/dtensor-jobs

--- get-clients.py
+++ get-clients.py
@@ -1,9 +1,19 @@
+"""Get client id / client num from DTENSOR_JOBS env.
+
+Syntax:
+
+  get-client.py:
+    returns total number of clients (e.g. 4), suitable for DTENSOR_NUM_CLIENTS
+
+  get-client.py hostname: returns client id for clients on host `hostname`, 
+    e.g. 0 1 2 3. Suitable for DTENSOR_NUM_CLIENTS. usually use with a for
+    loop in bash.
+"""
 import os
 import sys
 
 
 dtensor_jobs = os.environ['DTENSOR_JOBS']
-
 dtensor_jobs = dtensor_jobs.split(',')
 
 if len(sys.argv) == 2:
--- run-multi-nodes.sh
+++ run-multi-nodes.sh
@@ -1,8 +1,12 @@
 HOSTNAME=`hostname`
+DTENSOR_JOBS_FILE=/etc/dtensor-jobs
+if ! [[ -f "${DTENSOR_JOBS_FILE}" ]]; then
+  echo "${DTENSOR_JOBS_FILE}" is missing. Ensure the cluster is bootstraped.
+fi
 
 export TF_CPP_MIN_LOG_LEVEL=3
 export DTENSOR_JOB_NAME=worker
-export DTENSOR_JOBS=`cat dtensor-jobs`
+export DTENSOR_JOBS=`cat ${DTENSOR_JOBS_FILE}`
 export DTENSOR_NUM_CLIENTS=`python get-clients.py`
 
 for i in `python get-clients.py`; do
