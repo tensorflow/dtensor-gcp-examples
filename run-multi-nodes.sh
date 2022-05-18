a439915b0037553c8d1736a109d501d02ca66763
Yu Feng <feyu@google.com>
2022-05-18 00:44:35 +0000

    fix dtensor-jobs parsing.

--- run-multi-nodes.sh
+++ run-multi-nodes.sh
@@ -1,15 +1,16 @@
-HOSTNAME=`hostname`
 DTENSOR_JOBS_FILE=/etc/dtensor-jobs
 if ! [[ -f "${DTENSOR_JOBS_FILE}" ]]; then
   echo "${DTENSOR_JOBS_FILE}" is missing. Ensure the cluster is bootstraped.
 fi
+content=$(printf "%s," $(cat ${DTENSOR_JOBS_FILE}))
+export DTENSOR_JOBS="${content%,}"
 
+HOSTNAME=$(hostname)
 export TF_CPP_MIN_LOG_LEVEL=3
 export DTENSOR_JOB_NAME=worker
-export DTENSOR_JOBS=`cat ${DTENSOR_JOBS_FILE}`
 export DTENSOR_NUM_CLIENTS=`python get-clients.py`
 
-for i in `python get-clients.py`; do
+for i in $(python get-clients.py ${HOSTNAME}); do
   export DTENSOR_CLIENTS_ID="$i"
   python dtensor-client.py
 done
