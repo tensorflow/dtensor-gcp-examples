f83e3f4d5db8c1df8e9e8ecc0a34f6404c9ba5c2
Yu Feng <feyu@google.com>
2022-05-18 17:04:30 +0000

    enable logging.

--- run-multi-nodes.sh
+++ run-multi-nodes.sh
@@ -6,7 +6,7 @@ content=$(printf "%s," $(cat ${DTENSOR_JOBS_FILE}))
 export DTENSOR_JOBS="${content%,}"
 
 HOSTNAME=$(hostname)
-export TF_CPP_MIN_LOG_LEVEL=3
+# export TF_CPP_MIN_LOG_LEVEL=3
 export DTENSOR_JOB_NAME=worker
 export DTENSOR_NUM_CLIENTS=`python get-clients.py`
 
